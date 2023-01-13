038f7f9bf7e84a8dfaf1c8dddb0f97bbf3d5edc9
isaac-tfn <isaac.scott@transportforthenorth.com>
2023-01-13 11:29:48 +0000

    changed from abspath to relpath

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(r'src\caf\space'))
+sys.path.insert(0, os.path.relpath(r'src\caf\space'))
 
 project = 'caf.space'
 copyright = '2023, TfN'
