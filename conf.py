5c7765d5c80dd9f837723a6e84a149284c62e9d2
Isaac Scott <120399057+isaac-tfn@users.noreply.github.com>
2023-01-13 11:49:35 +0000

    update conf.py

--- conf.py
+++ conf.py
@@ -62,7 +62,7 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
-master_doc = 'index'
+master_doc = 'doc/index'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
