44e6bf0d827a2990d1283e3bd89b509950112ac0
Isaac Scott <isaac.scott@transportforthenorth.com>
2023-03-23 08:56:34 +0000

    Adding empty ui script

---
+++ src/caf/space/ui.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+User interface for caf.space
+"""
+# Built-Ins
+
+# Third Party
+
+# Local Imports
+# pylint: disable=import-error,wrong-import-position
+# Local imports here
+# pylint: enable=import-error,wrong-import-position
+
+# # # CONSTANTS # # #
+
+# # # CLASSES # # #
+
+# # # FUNCTIONS # # #
