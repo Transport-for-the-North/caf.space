d952de13ef33193a0bc5ffe48775acce25cb7cdd
Isaac Scott <isaac.scott@transportforthenorth.com>
2025-02-20 13:27:29 +0000

    Changed mode on validators to after
    Ensures 'values' will always be an instance of the class itself, rather than a dict containing values.

--- src/caf/space/inputs.py
+++ src/caf/space/inputs.py
@@ -60,13 +60,13 @@ class ZoneSystemInfo(BaseConfig):
     shapefile: Path
     id_col: str
 
-    @model_validator(mode="before")
+    @model_validator(mode="after")
     def _id_col_in_file(cls, values):
-        with fiona.collection(values["shapefile"]) as source:
+        with fiona.collection(values.shapefile) as source:
             schema = source.schema
-            if values["id_col"] not in schema["properties"].keys():
+            if values.id_col not in schema["properties"].keys():
                 raise ValueError(
-                    f"The id_col provided, {values['id_col']}, does not appear"
+                    f"The id_col provided, {values.id_col}, does not appear"
                     f" in the given shapefile. Please choose from:"
                     f"{schema['properties'].keys()}."
                 )
@@ -136,10 +136,10 @@ class LowerZoneSystemInfo(ZoneSystemInfo):
             raise FileNotFoundError(f"The weight data path provided for {v} does not exist.")
         return v
 
-    @model_validator(mode="before")
+    @model_validator(mode="after")
     def _valid_data_col(cls, values):
-        cols = pd.read_csv(values["weight_data"], nrows=1).columns
-        for v in [values["data_col"], values["weight_id_col"]]:
+        cols = pd.read_csv(values.weight_data, nrows=1).columns
+        for v in [values.data_col, values.weight_id_col]:
             if v not in cols:
                 raise ValueError(f"The given col, {v}, does not appear in the weight data.")
         return values
