885e91807d58b8f7ab36723c5f0d778e404d85f2
Isaac Scott <isaac.scott@transportforthenorth.com>
2025-05-29 08:59:56 +0100

    Refactor main code for speed

--- src/caf/space/line_to_line.py
+++ src/caf/space/line_to_line.py
@@ -39,6 +39,7 @@ class LinkInfo:
 
 
 def calc_angle(line):
+    """Calculate the angle of a line in degrees."""
     start = shapely.get_point(line, 0)
     end = shapely.get_point(line, -1)
     x = end.x - start.x
@@ -47,6 +48,7 @@ def calc_angle(line):
 
 
 def relative_angle(feat_1, feat_2):
+    """Calculate the relative angle between two features."""
     if isinstance(feat_1, shapely.LineString):
         angle_1 = calc_angle(feat_1)
     else:
@@ -62,6 +64,7 @@ def relative_angle(feat_1, feat_2):
 
 
 def rmse(line_1: shapely.LineString, line_2: shapely.LineString, sections: int = 10):
+    """Calculate the root mean square distance between two lines."""
     points = []
     for seg in range(sections + 1):
         points.append(
@@ -75,6 +78,7 @@ def rmse(line_1: shapely.LineString, line_2: shapely.LineString, sections: int =
 
 
 def project_line(longer, shorter_start, shorter_end):
+    """Project a line onto another line, returning the last segment."""
     start = longer.interpolate(longer.project(shorter_start))
     end = longer.interpolate(longer.project(shorter_end))
     line_frag = shapely.ops.split(longer, end.buffer(0.01)).geoms[0]
@@ -87,31 +91,66 @@ def project_line(longer, shorter_start, shorter_end):
 
 
 def preprocess(link_info: LinkInfo, buffer_dist=50, crs="EPSG:27700"):
+    """
+    Preprocess a link.
+
+    Sets the crs to the input and then calculates some metrics.
+
+    Returned GeoDataFrame contains:
+
+    - start: Start point of the link geometry
+    - end: End point of the link geometry
+    - angle: Angle of the link geometry in degrees
+    - buffer: Buffered geometry of the link
+    - crow_fly: Crow fly distance between start and end points
+    - straightness: Ratio of crow fly distance to link length
+    - geometry: Original link geometry
+    """
     gdf = link_info.gdf
     if gdf.crs != crs:
         gdf.to_crs(crs, inplace=True)
     inner = gdf.copy()
     if isinstance(link_info.identifier, list):
-        inner['id'] = inner[link_info.identifier[0]].astype(str) + '_' + inner[link_info.identifier[1]].astype(str)
+        inner["id"] = (
+            inner[link_info.identifier[0]].astype(str)
+            + "_"
+            + inner[link_info.identifier[1]].astype(str)
+        )
     else:
-        inner.rename(columns={link_info.identifier: 'id'}, inplace=True)
-    inner.set_index('id', inplace=True)
+        inner.rename(columns={link_info.identifier: "id"}, inplace=True)
+    inner.set_index("id", inplace=True)
     inner["start"] = shapely.get_point(inner.geometry, 0)
     inner["end"] = shapely.get_point(inner.geometry, -1)
     # MultiLineStrings break this - temp fix should be looked into
-    inner = inner[~inner['end'].isna()]
+    inner = inner[~inner["end"].isna()]
     inner["angle"] = inner.geometry.apply(calc_angle)
     inner["buffer"] = inner.buffer(buffer_dist, cap_style="flat")
     inner["crow_fly"] = shapely.get_point(inner.geometry, 0).distance(
         shapely.get_point(inner.geometry, -1)
     )
+    inner['length'] = inner.geometry.length
     inner["straightness"] = inner["crow_fly"] / inner.length
     return inner[
-        ["start", "end", "angle", "buffer", "crow_fly", "straightness", "geometry"]
+        ["start", "end", "angle", "buffer", "crow_fly", "length", "straightness", "geometry"]
     ].sort_index()
 
 
 def init_join(targ, ref, angle_threshold=60):
+    """
+    Initial join between target and reference links.
+
+    This will join all links in ref within the buffer of targ, and filter for the
+    relative angle between them to be less than the angle_threshold.
+    Parameters
+    ----------
+    targ
+    ref
+    angle_threshold
+
+    Returns
+    -------
+
+    """
     joined = gpd.GeoDataFrame(targ[["angle", "straightness"]], geometry=targ["buffer"]).sjoin(
         ref[["angle", "geometry"]]
     )
@@ -127,138 +166,80 @@ def init_join(targ, ref, angle_threshold=60):
     ]
 
 
-def find_con(longer, shorter):
-    line = project_line(longer.geometry, shorter.start, shorter.end)
-    shorter_angle = shorter.angle
-    shorter_geometry = shorter.geometry
-    if line.length < shorter.geometry.length * 0.9:
-        # if line.length < shorter.geometry.length / 2:
-        #     return ConvergenceValues(np.inf, 0, 0, 0)
+def find_con(longer, shorter, longer_suffix: str="", shorter_suffix: str = ""):
+    """Find convergence between two links."""
+    line = project_line(longer[f"geometry{longer_suffix}"], shorter[f"start{shorter_suffix}"], shorter[f"end{shorter_suffix}"])
+    shorter_angle = shorter[f"angle{shorter_suffix}"]
+    shorter_geometry = shorter[f"geometry{shorter_suffix}"]
+    if line.length < shorter_geometry.length * 0.9:
         shorter_geometry = project_line(
             shorter_geometry, shapely.get_point(line, 0), shapely.get_point(line, -1)
         )
         shorter_angle = calc_angle(shorter_geometry)
     score = rmse(line, shorter_geometry, 10)
     angle = relative_angle(line, shorter_angle)
-    # dis = line.distance(shorter.geometry)
-    # haus_dis = line.hausdorff_distance(shorter.geometry)
-    match_len = shorter["crow_fly"]
+    shorter_len = shorter[f"crow_fly{shorter_suffix}"]
+    longer_len = longer[f"crow_fly{longer_suffix}"]
     full_len = shorter_geometry.length
-    return ConvergenceValues(score, match_len, full_len, angle)
+    return pd.Series([score, shorter_len, longer_len, shorter[f"geometry{shorter_suffix}"].length, longer[f"geometry{longer_suffix}"].length, full_len, angle], index=["distance", f"crow_fly{shorter_suffix}", f"crow_fly{longer_suffix}", f"length{shorter_suffix}", f"length{longer_suffix}", "overlap_length", "angle"])
 
 
-def main(
-    ref_links: LinkInfo,
-    target_links: LinkInfo,
-    filter_out: bool = False
-):
+def main(ref_links: LinkInfo, target_links: LinkInfo, filter_out: bool = False):
     target_processed = preprocess(target_links)
+    target_processed.index.name = 'id_targ'
     ref_processed = preprocess(ref_links)
+    ref_processed.index.name = 'id_ref'
     joined = init_join(target_processed, ref_processed)
     missing_targ = target_processed.drop(joined.index.unique())
-    out_out = {}
-    actual_length = {}
-    for multi in tqdm(joined.index.unique()):
-        feature = target_processed.loc[multi]
-        links_iter = joined.loc[multi]
-        out = {}
-        if isinstance(links_iter, pd.DataFrame):
-            for link in links_iter.iterrows():
-                link = link[1]
-                ref = [link["id_ref"]]
-                refs = ref
-                straightness = link["straightness"]
-                ref_link = ref_processed.loc[refs]
-                ref_link = ref_link.squeeze()
-                if ref_link.geometry.length > feature.geometry.length:
-                    stats = find_con(ref_link, feature)
-                else:
-                    stats = find_con(feature, ref_link)
-                if len(refs) == 1:
-                    refs = refs[0]
-                out[refs] = (
-                    stats.rmse,
-                    stats.full_length,
-                    stats.crow_fly_length,
-                    stats.angle,
-                    stats.angle,
-                    straightness,
-                )
-        else:
-            ref = [links_iter["id_ref"]]
-            straightness = links_iter["straightness"]
-            ref_link = ref_processed.loc[ref]
-            ref_link = ref_link.squeeze()
-            if ref_link.geometry.length > feature.geometry.length:
-                stats = find_con(ref_link, feature)
-            else:
-                stats = find_con(feature, ref_link)
-            if len(ref) == 1:
-                ref = ref[0]
-            out[ref] = (
-                stats.rmse,
-                stats.full_length,
-                stats.crow_fly_length,
-                stats.angle,
-                stats.angle,
-                straightness,
-            )
-        out = pd.DataFrame.from_dict(
-            out,
-            orient="index",
-            columns=[
-                "convergence",
-                "ref_length",
-                "ref_crow_fly",
-                "segment_angle",
-                "angle",
-                "targ_straightness",
-            ],
-        )
-        if len(multi) == 1:
-            multi = multi[0]
-        out.sort_values(by='convergence', inplace=True)
-        if filter_out:
-            temp_length = 0
-            out_ind = []
-            for idx in range(len(out)):
-                temp_length += out.iloc[idx]['ref_length']
-                if temp_length < feature.geometry.length:
-                    out_ind.append(out.index[idx])
-                else:
-                    break
-            # filtered = out[out["convergence"] == out["convergence"].min()]
-            out_out[multi] = out.loc[out_ind]
-        else:
-            out_out[multi] = out
-        actual_length[multi] = feature.geometry.length
+    big_join = target_processed.join(joined["id_ref"]).set_index('id_ref', append=True).join(ref_processed, lsuffix='_targ', rsuffix='_ref')
+    ref_first = big_join.loc[big_join['length_targ'] < big_join['length_ref']]
+    targ_first = big_join.loc[big_join['length_targ'] >= big_join['length_ref']]
+    results_a = ref_first.apply(
+        lambda row: find_con(
+            row.loc[["geometry_ref", "crow_fly_ref"]],
+            row.loc[["geometry_targ", "angle_targ", "start_targ", "end_targ", "crow_fly_targ"]],
+            '_ref', '_targ'
+        ),
+        axis=1,
+    )
+    results_b = targ_first.apply(
+        lambda row: find_con(
+            row.loc[["geometry_targ", "crow_fly_targ"]],
+            row.loc[["geometry_ref", "angle_ref", "start_ref", "end_ref", "crow_fly_ref"]],
+            '_targ', '_ref'
+        ),
+        axis=1,
+    )
+    results = pd.concat([results_a, results_b])
+    return results.reset_index(level='id_targ').sort_values(by=['id_targ', 'distance']).set_index('id_targ', append=True)
 
-    actual_length = pd.Series(actual_length, name='target_link_length')
-    df = pd.concat(out_out).reset_index()
-    df.set_index(['level_0', 'level_1'], inplace=True)
-    df.index.names = ['targ', 'ref']
-    actual_length.index.name = 'targ'
-    df = actual_length.to_frame().join(df)
-    df['overlap'] = df['ref_length'] / df['target_link_length']
-    return df, missing_targ
 
 def process_missing(lookup, gdf, threshold):
     # Completely missing will be assigned np.inf. User can decide what
     # threshold to try another method beneath.
-    missing_ind = lookup[lookup['convergence'] > threshold].index
+    missing_ind = lookup[lookup["convergence"] > threshold].index
     missing = gdf.loc[missing_ind].copy()
     matching = gdf.drop(missing_ind).copy()
 
 
 if __name__ == "__main__":
     home_dir = Path(r"E:\tmjt_data\out\lookup")
-    itn = gpd.read_file(r"O:\10.Internal_Requests\24 MRNmatchingNoHAM2023\MRN\mrnpaths.shp", engine='pyogrio')
+    itn = gpd.read_file(
+        r"O:\10.Internal_Requests\24 MRNmatchingNoHAM2023\MRN\mrnpaths.shp", engine="pyogrio"
+    )
     # itn = itn[itn["rdclass"] != "ZC"]
     # osm = gpd.read_file(home_dir / "manchester_os").set_index('unique_id')
     # osm = LinkInfo(gdf=osm, identifier='unique_id', name='osm')
-    itn = LinkInfo(gdf=itn, identifier='path_id', name='mrn')
-    noham = gpd.read_file(r"O:\10.Internal_Requests\24 MRNmatchingNoHAM2023\NoHAM\NoHAM_Base.shp", engine='pyogrio')
-    noham = LinkInfo(gdf=noham[(noham['A']>10000) & (noham['B']>10000)], identifier=['A','B'], name='noham')
-    out, missing = main(noham, itn, True)
+    itn = LinkInfo(gdf=itn, identifier="path_id", name="mrn")
+    noham = gpd.read_file(
+        r"O:\10.Internal_Requests\24 MRNmatchingNoHAM2023\NoHAM\NoHAM_Base.shp",
+        engine="pyogrio",
+    )
+    noham = LinkInfo(
+        gdf=noham[(noham["A"] > 10000) & (noham["B"] > 10000)],
+        identifier=["A", "B"],
+        name="noham",
+    )
+    out = main(noham, itn, True)
     out.to_csv(home_dir / "sat_rami_lookup.csv")
-    print('debugging')
+    print("debugging")
