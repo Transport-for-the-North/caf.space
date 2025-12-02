d7f45235b8656b976114dda0e83a1c74c5e766b3
Isaac Scott <isaac.scott@transportforthenorth.com>
2025-12-02 12:18:23 +0000

    Fixing docs

--- examples/weighted_run.py
+++ examples/weighted_run.py
@@ -1,34 +1,37 @@
+"""
+Example of weighted centroid run
+================================
+"""
+
 import caf.space as cs
 from pathlib import Path
 
 lower = cs.LowerZoneSystemInfo(
     name="pop",
-    shapefile=Path(r"I:\Airport demand\Alok\shapefiles\pop_weighted.gpkg"),
+    shapefile=Path("dir/pop_weighted.gpkg"),
     id_col="lsoa21cd",
     data_col="pop_weighted",
     weight_data_year=2021,
 )
 
-buffer = cs.TransZoneSystemInfo(
-    name="buffer",
-    shapefile=r"I:\Airport demand\Alok\shapefiles\buffere.gpkg",
-    id_col="band_km",
+zone_1 = cs.TransZoneSystemInfo(
+    name="zone_1",
+    shapefile=r"dir/zone_1.gpkg",
+    id_col="id",
 )
 
-gor = cs.TransZoneSystemInfo(
-    name="gor", shapefile=r"I:\Airport demand\Alok\shapefiles\zone.gpkg", id_col="OBJECTID"
+zone_2 = cs.TransZoneSystemInfo(
+    name="zone_2",
+    shapefile=r"dir/zone_2.gpkg",
+    id_col="id",
 )
 
 params = cs.ZoningTranslationInputs(
-    zone_1=gor,
-    zone_2=buffer,
+    zone_1=zone_1,
+    zone_2=zone_2,
     lower_zoning=lower,
-    output_path=r"I:\Airport demand\Alok\shapefiles",
-    cache_path=r"I:\Airport demand\Alok\shapefiles",
-    method="gaussian_pop",
-    tolerance=0.99,
+    method="pop",
     rounding=True,
 )
-x = cs.ZoneTranslation(params)
-y = x.weighted_translation()
-print("debugging")
+trans_class = cs.ZoneTranslation(params)
+trans_df = trans.weighted_translation()
