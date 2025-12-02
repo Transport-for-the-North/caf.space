35fd51f420fc44c5f44f9577deae1f16b74da0ce
Isaac Scott <isaac.scott@transportforthenorth.com>
2025-12-02 08:49:21 +0000

    black examples

--- examples/weighted_run.py
+++ examples/weighted_run.py
@@ -9,24 +9,26 @@ lower = cs.LowerZoneSystemInfo(
     weight_data_year=2021,
 )
 
-buffer = cs.TransZoneSystemInfo(name='buffer',
-                                shapefile=r"I:\Airport demand\Alok\shapefiles\buffere.gpkg",
-                                id_col='band_km')
+buffer = cs.TransZoneSystemInfo(
+    name="buffer",
+    shapefile=r"I:\Airport demand\Alok\shapefiles\buffere.gpkg",
+    id_col="band_km",
+)
 
-gor = cs.TransZoneSystemInfo(name='gor',
-                                shapefile=r"I:\Airport demand\Alok\shapefiles\zone.gpkg",
-                                id_col='OBJECTID')
+gor = cs.TransZoneSystemInfo(
+    name="gor", shapefile=r"I:\Airport demand\Alok\shapefiles\zone.gpkg", id_col="OBJECTID"
+)
 
 params = cs.ZoningTranslationInputs(
     zone_1=gor,
     zone_2=buffer,
     lower_zoning=lower,
-    output_path= r"I:\Airport demand\Alok\shapefiles",
+    output_path=r"I:\Airport demand\Alok\shapefiles",
     cache_path=r"I:\Airport demand\Alok\shapefiles",
     method="gaussian_pop",
     tolerance=0.99,
     rounding=True,
 )
-x= cs.ZoneTranslation(params)
+x = cs.ZoneTranslation(params)
 y = x.weighted_translation()
-print('debugging')
+print("debugging")
